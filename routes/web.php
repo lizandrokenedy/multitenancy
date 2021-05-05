c49cb6a954e603a4dac72337cef27a51d28b8756
Lizandro Kenedy <lizandro.santos@grupolpj.com>
2021-05-04 21:10:25 -0400

    adicionando autenticaçao jeststream livewire

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ Route::view('/404-tenant', 'errors.404-tenant')->name('404.tenant');
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
