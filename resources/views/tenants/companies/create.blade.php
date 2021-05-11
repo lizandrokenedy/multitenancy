de149e38a06c673dccd12c4addb1305f7a6e37f5
Lizandro Kenedy <lizandro.santos@grupolpj.com>
2021-05-11 11:38:34 -0400

    Adicionando titulos empresas

--- resources/views/tenants/companies/create.blade.php
+++ resources/views/tenants/companies/create.blade.php
@@ -4,7 +4,7 @@
 
 @section('content_header')
     <div class="container-fluid">
-        <div class="d-flex justify-content-end">
+        <div class="d-flex justify-content-between">
             <h5>Criar Empresa</h5>
             <x-breadcrumb :items="$items" />
         </div>
