c1192e27ede07b60c1191aa55123c20471d83643
Lizandro Kenedy <lizandro.santos@grupolpj.com>
2021-05-14 22:10:25 -0400

    Estrutura inicial menus

--- resources/views/vendor/adminlte/partials/sidebar/tenant-sidebar.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/tenant-sidebar.blade.php
@@ -72,3 +72,43 @@
         </li>
     </ul>
 </li>
+
+<li class="nav-item {{ Route::is('tenants.users*', 'tenants.roles*', 'tenants.permissions*') ? 'menu-open' : '' }}">
+    <a href="#"
+        class="nav-link {{ Route::is('tenants.users*', 'tenants.roles*', 'tenants.permissions*') ? 'active' : '' }}">
+        <i class="fa fa-file-csv mr-1"></i>
+        <p>
+            Importações
+            <i class="right fas fa-angle-left"></i>
+        </p>
+    </a>
+    <ul class="nav nav-treeview">
+        <li class="nav-item">
+            <a class="nav-link {{ Route::is('tenants.users*') ? 'active' : '' }}"
+                href="{{ route('tenants.users.index') }}">
+                <i class="fa fa-users mr-1 ml-2"></i>
+                <p>
+                   Escolas
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ Route::is('tenants.roles*') ? 'active' : '' }}"
+                href="{{ route('tenants.roles.index') }}">
+                <i class="fa fa-user-lock mr-1 ml-2"></i>
+                <p>
+                    Professores
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ Route::is('tenants.permissions*') ? 'active' : '' }}"
+                href="{{ route('tenants.permissions.index') }}">
+                <i class="fa fa-unlock mr-1 ml-2"></i>
+                <p>
+                    Alunos
+                </p>
+            </a>
+        </li>
+    </ul>
+</li>
--- routes/tenant.php
+++ routes/tenant.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Tenant\GradeController;
 use App\Http\Controllers\Tenant\PermissionController;
 use App\Http\Controllers\Tenant\RoleController;
+use App\Http\Controllers\Tenant\SchoolController;
+use App\Http\Controllers\Tenant\TeacherController;
 use App\Http\Controllers\Tenant\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -25,20 +28,20 @@ Route::group(['middleware' => ['auth']], function () {
     });
 
     #Schools
-    Route::resource('schools', PermissionController::class);
+    Route::resource('schools', SchoolController::class);
     Route::group(['prefix' => 'schools', 'name' => 'schools'], function () {
-        Route::post('list-all', [PermissionController::class, 'listAll'])->name('list-all');
+        Route::post('list-all', [SchoolController::class, 'listAll'])->name('list-all');
     });
 
     #Teachers
-    Route::resource('teachers', PermissionController::class);
+    Route::resource('teachers', TeacherController::class);
     Route::group(['prefix' => 'teachers', 'name' => 'teachers'], function () {
-        Route::post('list-all', [PermissionController::class, 'listAll'])->name('list-all');
+        Route::post('list-all', [TeacherController::class, 'listAll'])->name('list-all');
     });
 
     #Grades
-    Route::resource('grades', PermissionController::class);
+    Route::resource('grades', GradeController::class);
     Route::group(['prefix' => 'grades', 'name' => 'grades'], function () {
-        Route::post('list-all', [PermissionController::class, 'listAll'])->name('list-all');
+        Route::post('list-all', [GradeController::class, 'listAll'])->name('list-all');
     });
 });
