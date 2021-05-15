3147a5dd4ac2ed9f578d7a357194664cc4afec26
Lizandro Kenedy <lizandro.santos@grupolpj.com>
2021-05-15 13:30:04 -0400

    criando rotas e controllers

--- resources/views/vendor/adminlte/partials/sidebar/tenant-sidebar.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/tenant-sidebar.blade.php
@@ -1,5 +1,5 @@
 <li class="nav-item">
-    <a class="nav-link {{ Route::is('tenants.users*') ? 'active' : '' }}" href="{{ route('tenants.users.index') }}">
+    <a class="nav-link {{ Route::is('tenants.schools*') ? 'active' : '' }}" href="{{ route('tenants.schools.index') }}">
         <i class="fa fa-building mr-1"></i>
         <p>
             Escolas
@@ -7,7 +7,7 @@
     </a>
 </li>
 <li class="nav-item">
-    <a class="nav-link {{ Route::is('tenants.roles*') ? 'active' : '' }}" href="{{ route('tenants.roles.index') }}">
+    <a class="nav-link {{ Route::is('tenants.teachers*') ? 'active' : '' }}" href="{{ route('tenants.teachers.index') }}">
         <i class="fa fa-graduation-cap mr-1"></i>
         <p>
             Professores
@@ -15,8 +15,8 @@
     </a>
 </li>
 <li class="nav-item">
-    <a class="nav-link {{ Route::is('tenants.permissions*') ? 'active' : '' }}"
-        href="{{ route('tenants.permissions.index') }}">
+    <a class="nav-link {{ Route::is('tenants.students*') ? 'active' : '' }}"
+        href="{{ route('tenants.students.index') }}">
         <i class="fa fa-user-graduate mr-1"></i>
         <p>
             Alunos
@@ -24,8 +24,8 @@
     </a>
 </li>
 <li class="nav-item">
-    <a class="nav-link {{ Route::is('tenants.permissions*') ? 'active' : '' }}"
-        href="{{ route('tenants.permissions.index') }}">
+    <a class="nav-link {{ Route::is('tenants.grades*') ? 'active' : '' }}"
+        href="{{ route('tenants.grades.index') }}">
         <i class="fa fa-clipboard mr-1"></i>
         <p>
             Notas
@@ -33,8 +33,8 @@
     </a>
 </li>
 <li class="nav-item">
-    <a class="nav-link {{ Route::is('tenants.permissions*') ? 'active' : '' }}"
-        href="{{ route('tenants.permissions.index') }}">
+    <a class="nav-link {{ Route::is('tenants.assessments*') ? 'active' : '' }}"
+        href="{{ route('tenants.assessments.index') }}">
         <i class="fa fa-clipboard mr-1"></i>
         <p>
             Avaliações
@@ -82,9 +82,9 @@
     </ul>
 </li>
 
-<li class="nav-item {{ Route::is('tenants.users*', 'tenants.roles*', 'tenants.permissions*') ? 'menu-open' : '' }}">
+<li class="nav-item {{ Route::is('tenants.imports*') ? 'menu-open' : '' }}">
     <a href="#"
-        class="nav-link {{ Route::is('tenants.users*', 'tenants.roles*', 'tenants.permissions*') ? 'active' : '' }}">
+        class="nav-link {{ Route::is('tenants.imports*') ? 'active' : '' }}">
         <i class="fa fa-file-csv mr-1"></i>
         <p>
             Importações
@@ -93,8 +93,8 @@
     </a>
     <ul class="nav nav-treeview">
         <li class="nav-item">
-            <a class="nav-link {{ Route::is('tenants.users*') ? 'active' : '' }}"
-                href="{{ route('tenants.users.index') }}">
+            <a class="nav-link {{ Route::is('tenants.imports.schools*') ? 'active' : '' }}"
+                href="{{ route('tenants.imports.schools.index') }}">
                 <i class="fa fa-users mr-1 ml-2"></i>
                 <p>
                    Escolas
@@ -102,8 +102,8 @@
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{ Route::is('tenants.roles*') ? 'active' : '' }}"
-                href="{{ route('tenants.roles.index') }}">
+            <a class="nav-link {{ Route::is('tenants.imports.teachers*') ? 'active' : '' }}"
+                href="{{ route('tenants.imports.teachers.index') }}">
                 <i class="fa fa-user-lock mr-1 ml-2"></i>
                 <p>
                     Professores
@@ -111,8 +111,8 @@
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{ Route::is('tenants.permissions*') ? 'active' : '' }}"
-                href="{{ route('tenants.permissions.index') }}">
+            <a class="nav-link {{ Route::is('tenants.imports.students*') ? 'active' : '' }}"
+                href="{{ route('tenants.imports.students.index') }}">
                 <i class="fa fa-unlock mr-1 ml-2"></i>
                 <p>
                     Alunos
--- routes/tenant.php
+++ routes/tenant.php
@@ -1,47 +1,79 @@
 <?php
 
+use App\Http\Controllers\Tenant\AssessmentController;
 use App\Http\Controllers\Tenant\GradeController;
 use App\Http\Controllers\Tenant\PermissionController;
 use App\Http\Controllers\Tenant\RoleController;
 use App\Http\Controllers\Tenant\SchoolController;
+use App\Http\Controllers\Tenant\StudentController;
 use App\Http\Controllers\Tenant\TeacherController;
 use App\Http\Controllers\Tenant\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth']], function () {
-    #Users
-    Route::resource('users', UserController::class);
-    Route::group(['prefix' => 'users', 'name' => 'users'], function () {
-        Route::post('list-all', [UserController::class, 'listAll'])->name('list-all');
-    });
-
-    #Roles
-    Route::resource('roles', RoleController::class);
-    Route::group(['prefix' => 'roles', 'name' => 'roles'], function () {
-        Route::post('list-all', [RoleController::class, 'listAll'])->name('list-all');
-    });
-
-    #Permissions
-    Route::resource('permissions', PermissionController::class);
-    Route::group(['prefix' => 'permissions', 'name' => 'permissions'], function () {
-        Route::post('list-all', [PermissionController::class, 'listAll'])->name('list-all');
-    });
-
-    #Schools
+    #Schools/Escolas
     Route::resource('schools', SchoolController::class);
-    Route::group(['prefix' => 'schools', 'name' => 'schools'], function () {
+    Route::group(['prefix' => 'schools', 'as' => 'schools.'], function () {
         Route::post('list-all', [SchoolController::class, 'listAll'])->name('list-all');
     });
 
-    #Teachers
+    #Teachers/Professores
     Route::resource('teachers', TeacherController::class);
-    Route::group(['prefix' => 'teachers', 'name' => 'teachers'], function () {
+    Route::group(['prefix' => 'teachers', 'as' => 'teachers.'], function () {
         Route::post('list-all', [TeacherController::class, 'listAll'])->name('list-all');
     });
 
-    #Grades
+    #Students/Alunos
+    Route::resource('students', StudentController::class);
+    Route::group(['prefix' => 'students', 'as' => 'students.'], function () {
+        Route::post('list-all', [StudentController::class, 'listAll'])->name('list-all');
+    });
+
+    #Grades/Notas
     Route::resource('grades', GradeController::class);
-    Route::group(['prefix' => 'grades', 'name' => 'grades'], function () {
+    Route::group(['prefix' => 'grades', 'as' => 'grades.'], function () {
         Route::post('list-all', [GradeController::class, 'listAll'])->name('list-all');
+    });
+
+    #Assessments/Avaliações
+    Route::resource('assessments', AssessmentController::class);
+    Route::group(['prefix' => 'assessments', 'as' => 'assessments.'], function () {
+        Route::post('list-all', [AssessmentController::class, 'listAll'])->name('list-all');
+    });
+
+    #Users/Usuários
+    Route::resource('users', UserController::class);
+    Route::group(['prefix' => 'users', 'as' => 'users.'], function () {
+        Route::post('list-all', [UserController::class, 'listAll'])->name('list-all');
+    });
+
+    #Roles/Perfis
+    Route::resource('roles', RoleController::class);
+    Route::group(['prefix' => 'roles', 'as' => 'roles.'], function () {
+        Route::post('list-all', [RoleController::class, 'listAll'])->name('list-all');
+    });
+
+    #Permissions/Permissões
+    Route::resource('permissions', PermissionController::class);
+    Route::group(['prefix' => 'permissions', 'as' => 'permissions.'], function () {
+        Route::post('list-all', [PermissionController::class, 'listAll'])->name('list-all');
+    });
+
+    #Imports/Importações
+    Route::group(['prefix' => 'imports', 'as' => 'imports.'], function () {
+        Route::group(['prefix' => 'schools', 'as' => 'schools.'], function () {
+            Route::get('index', [SchoolController::class, 'viewImport'])->name('index');
+            Route::post('import', [SchoolController::class, 'import'])->name('import');
+        });
+
+        Route::group(['prefix' => 'teachers', 'as' => 'teachers.'], function () {
+            Route::get('index', [TeacherController::class, 'viewImport'])->name('index');
+            Route::post('import', [TeacherController::class, 'import'])->name('import');
+        });
+
+        Route::group(['prefix' => 'students', 'as' => 'students.'], function () {
+            Route::get('index', [StudentController::class, 'viewImport'])->name('index');
+            Route::post('import', [StudentController::class, 'import'])->name('import');
+        });
     });
 });
