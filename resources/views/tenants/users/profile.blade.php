bb6bdce744637f0ae589925838bb925c2383b72b
Lizandro Kenedy <lizandro.santos@grupolpj.com>
2021-06-19 17:22:32 -0400

    melhorando view profile

--- resources/views/tenants/users/profile.blade.php
+++ resources/views/tenants/users/profile.blade.php
@@ -25,7 +25,7 @@
                                         src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
                                 </div> --}}
 
-                                <h3 class="profile-username text-center">{{Auth::user()->name}}</h3>
+                                <h3 class="profile-username text-center">{{ Auth::user()->name }}</h3>
 
                                 <p class="text-muted text-center">Administrador Geral</p>
 
@@ -94,147 +94,51 @@
                         <div class="card">
                             <div class="card-header p-2">
                                 <ul class="nav nav-pills">
-                                    {{-- <li class="nav-item"><a class="nav-link" href="#activity" data-toggle="tab">Activity</a>
-                                    </li>
-                                    <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Timeline</a>
+                                    {{-- <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Timeline</a>
                                     </li> --}}
                                     <li class="nav-item"><a class="nav-link active" href="#settings"
                                             data-toggle="tab">Configurações da Conta</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#help" data-toggle="tab">Ajuda</a>
+                                    </li>
                                 </ul>
                             </div><!-- /.card-header -->
                             <div class="card-body">
                                 <div class="tab-content">
-                                    {{-- <div class="tab-pane" id="activity">
-                                        <!-- Post -->
-                                        <div class="post">
-                                            <div class="user-block">
-                                                <img class="img-circle img-bordered-sm"
-                                                    src="../../dist/img/user1-128x128.jpg" alt="user image">
-                                                <span class="username">
-                                                    <a href="#">Jonathan Burke Jr.</a>
-                                                    <a href="#" class="float-right btn-tool"><i
-                                                            class="fas fa-times"></i></a>
-                                                </span>
-                                                <span class="description">Shared publicly - 7:30 PM today</span>
-                                            </div>
-                                            <!-- /.user-block -->
-                                            <p>
-                                                Lorem ipsum represents a long-held tradition for designers,
-                                                typographers and the like. Some people hate it and argue for
-                                                its demise, but others ignore the hate as they create awesome
-                                                tools to help create filler text for everyone from bacon lovers
-                                                to Charlie Sheen fans.
-                                            </p>
-
-                                            <p>
-                                                <a href="#" class="link-black text-sm mr-2"><i
-                                                        class="fas fa-share mr-1"></i> Share</a>
-                                                <a href="#" class="link-black text-sm"><i class="far fa-thumbs-up mr-1"></i>
-                                                    Like</a>
-                                                <span class="float-right">
-                                                    <a href="#" class="link-black text-sm">
-                                                        <i class="far fa-comments mr-1"></i> Comments (5)
-                                                    </a>
-                                                </span>
-                                            </p>
-
-                                            <input class="form-control form-control-sm" type="text"
-                                                placeholder="Type a comment">
-                                        </div>
-                                        <!-- /.post -->
-
-                                        <!-- Post -->
-                                        <div class="post clearfix">
-                                            <div class="user-block">
-                                                <img class="img-circle img-bordered-sm"
-                                                    src="../../dist/img/user7-128x128.jpg" alt="User Image">
-                                                <span class="username">
-                                                    <a href="#">Sarah Ross</a>
-                                                    <a href="#" class="float-right btn-tool"><i
-                                                            class="fas fa-times"></i></a>
-                                                </span>
-                                                <span class="description">Sent you a message - 3 days ago</span>
-                                            </div>
-                                            <!-- /.user-block -->
-                                            <p>
-                                                Lorem ipsum represents a long-held tradition for designers,
-                                                typographers and the like. Some people hate it and argue for
-                                                its demise, but others ignore the hate as they create awesome
-                                                tools to help create filler text for everyone from bacon lovers
-                                                to Charlie Sheen fans.
-                                            </p>
-
-                                            <form class="form-horizontal">
-                                                <div class="input-group input-group-sm mb-0">
-                                                    <input class="form-control form-control-sm" placeholder="Response">
-                                                    <div class="input-group-append">
-                                                        <button type="submit" class="btn btn-danger">Send</button>
-                                                    </div>
+                                    <div class="tab-pane" id="help">
+                                        <form class="form-horizontal">
+                                            <div class="form-group row">
+                                                <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                                <div class="col-sm-10">
+                                                    <input type="email" class="form-control" id="inputName"
+                                                        value="{{ Auth::user()->name }}" placeholder="Name">
                                                 </div>
-                                            </form>
-                                        </div>
-                                        <!-- /.post -->
-
-                                        <!-- Post -->
-                                        <div class="post">
-                                            <div class="user-block">
-                                                <img class="img-circle img-bordered-sm"
-                                                    src="../../dist/img/user6-128x128.jpg" alt="User Image">
-                                                <span class="username">
-                                                    <a href="#">Adam Jones</a>
-                                                    <a href="#" class="float-right btn-tool"><i
-                                                            class="fas fa-times"></i></a>
-                                                </span>
-                                                <span class="description">Posted 5 photos - 5 days ago</span>
                                             </div>
-                                            <!-- /.user-block -->
-                                            <div class="row mb-3">
-                                                <div class="col-sm-6">
-                                                    <img class="img-fluid" src="../../dist/img/photo1.png" alt="Photo">
+                                            <div class="form-group row">
+                                                <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                                <div class="col-sm-10">
+                                                    <input type="email" class="form-control" id="inputEmail"
+                                                        value="{{ Auth::user()->email }}" placeholder="Email">
                                                 </div>
-                                                <!-- /.col -->
-                                                <div class="col-sm-6">
-                                                    <div class="row">
-                                                        <div class="col-sm-6">
-                                                            <img class="img-fluid mb-3" src="../../dist/img/photo2.png"
-                                                                alt="Photo">
-                                                            <img class="img-fluid" src="../../dist/img/photo3.jpg"
-                                                                alt="Photo">
-                                                        </div>
-                                                        <!-- /.col -->
-                                                        <div class="col-sm-6">
-                                                            <img class="img-fluid mb-3" src="../../dist/img/photo4.jpg"
-                                                                alt="Photo">
-                                                            <img class="img-fluid" src="../../dist/img/photo1.png"
-                                                                alt="Photo">
-                                                        </div>
-                                                        <!-- /.col -->
-                                                    </div>
-                                                    <!-- /.row -->
-                                                </div>
-                                                <!-- /.col -->
                                             </div>
-                                            <!-- /.row -->
 
-                                            <p>
-                                                <a href="#" class="link-black text-sm mr-2"><i
-                                                        class="fas fa-share mr-1"></i> Share</a>
-                                                <a href="#" class="link-black text-sm"><i class="far fa-thumbs-up mr-1"></i>
-                                                    Like</a>
-                                                <span class="float-right">
-                                                    <a href="#" class="link-black text-sm">
-                                                        <i class="far fa-comments mr-1"></i> Comments (5)
-                                                    </a>
-                                                </span>
-                                            </p>
+                                            <div class="form-group row">
+                                                <label for="inputExperience" class="col-sm-2 col-form-label">Descreva o
+                                                    Problema</label>
+                                                <div class="col-sm-10">
+                                                    <textarea class="form-control" id="inputExperience"
+                                                        placeholder="Experience"></textarea>
+                                                </div>
+                                            </div>
 
-                                            <input class="form-control form-control-sm" type="text"
-                                                placeholder="Type a comment">
-                                        </div>
-                                        <!-- /.post -->
+                                            <div class="form-group row">
+                                                <div class="offset-sm-2 col-sm-10">
+                                                    <button type="submit" class="btn btn-danger">Submit</button>
+                                                </div>
+                                            </div>
+                                        </form>
                                     </div>
                                     <!-- /.tab-pane -->
-                                    <div class="tab-pane" id="timeline">
+                                    {{-- <div class="tab-pane" id="timeline">
                                         <!-- The timeline -->
                                         <div class="timeline timeline-inverse">
                                             <!-- timeline time label -->
@@ -337,41 +241,35 @@
                                     <div class="tab-pane active" id="settings">
                                         <form class="form-horizontal">
                                             <div class="form-group row">
-                                                <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                                <label for="inputName" class="col-sm-2 col-form-label">Nome</label>
                                                 <div class="col-sm-10">
                                                     <input type="email" class="form-control" id="inputName"
-                                                        placeholder="Name">
+                                                        value="{{ Auth::user()->name }}" placeholder="Nome">
                                                 </div>
                                             </div>
                                             <div class="form-group row">
                                                 <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
                                                 <div class="col-sm-10">
                                                     <input type="email" class="form-control" id="inputEmail"
-                                                        placeholder="Email">
+                                                        value="{{ Auth::user()->email }}" placeholder="Email">
                                                 </div>
                                             </div>
                                             <div class="form-group row">
-                                                <label for="inputName2" class="col-sm-2 col-form-label">Name</label>
+                                                <label for="inputPassword" class="col-sm-2 col-form-label">Senha</label>
                                                 <div class="col-sm-10">
-                                                    <input type="text" class="form-control" id="inputName2"
-                                                        placeholder="Name">
+                                                    <input type="password" class="form-control" id="inputPassword"
+                                                        placeholder="Senha">
                                                 </div>
                                             </div>
                                             <div class="form-group row">
-                                                <label for="inputExperience"
-                                                    class="col-sm-2 col-form-label">Experience</label>
+                                                <label for="inputPasswordConfirm"
+                                                    class="col-sm-2 col-form-label">Confirmação Senha</label>
                                                 <div class="col-sm-10">
-                                                    <textarea class="form-control" id="inputExperience"
-                                                        placeholder="Experience"></textarea>
+                                                    <input type="password" class="form-control" id="inputPasswordConfirm"
+                                                        placeholder="Confirmação Senha">
                                                 </div>
                                             </div>
-                                            <div class="form-group row">
-                                                <label for="inputSkills" class="col-sm-2 col-form-label">Skills</label>
-                                                <div class="col-sm-10">
-                                                    <input type="text" class="form-control" id="inputSkills"
-                                                        placeholder="Skills">
-                                                </div>
-                                            </div>
+
                                             <div class="form-group row">
                                                 <div class="offset-sm-2 col-sm-10">
                                                     <div class="checkbox">
